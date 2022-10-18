39f592fe3eb1ae67d1cb323138f0a99ed364423a
Tony Park <tony@park-hagiwara.net>
2022-10-18 01:11:40 +0100

    Added template for help/about texts

---
+++ about_help_template.lua
@@ -0,0 +1,53 @@
+--[[
+You need a couple of lua table files called
+
+about.lua
+
+and  
+
+help.lua
+
+These are texts that are displayed when the 
+'about' and 'help' system menu options are 
+selected
+
+--]]
+
+text = 
+{
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+"About / Help",
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+"Some text here explaining",
+"about the project.",
+"",
+"Or possibly",
+"offering some help",
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+}
+
+return text
+
